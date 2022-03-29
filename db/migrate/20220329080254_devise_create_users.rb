8f9a64a9fd448b0cc9ab142c305756f07f3cfbb6
nagakihidenori <you_and_sunflowers@yahoo.co.jp>
2022-03-29 17:16:33 +0900

    migrationファイルにカラム名の記述

--- db/migrate/20220329080254_devise_create_users.rb
+++ db/migrate/20220329080254_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :email,              unique: true
       t.string :encrypted_password, null: false, default: ""
+      t.string :myouji,             null: false
+      t.string :namae,              null: false
+      t.string :kanamyouji,         null: false
+      t.string :kananamae,          null: false
+      t.date :birthday,             null: false
 
       ## Recoverable
       t.string   :reset_password_token

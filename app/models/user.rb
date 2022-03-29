4b7f233f1bf3f13945a73b9823b9a60d07dce253
nagakihidenori <you_and_sunflowers@yahoo.co.jp>
2022-03-29 18:26:39 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        validates :nickname, presence: true
+        validates_format_of :email, presence: true, uniqueness: true, with: ?=@
+        validates_format_of :encrypted_password, presence: true, length: { minimum: 6 }, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+        validates :myouji, presence: true
+        validates :namae, presence: true
+        validates :kanamyouji, presence: true
+        validates :kananamae, presence: true
+        validates :birthday, presence: true
 end

af0de8166c7d441fd2c6cfb5ca4258a96c5b0947
nagakihidenori <you_and_sunflowers@yahoo.co.jp>
2022-04-05 12:29:36 +0900

    Revert "Revert "コントローラーにアクション定義""
    This reverts commit 900b1d6191f7e432b5b123b663f9cd605da9269a.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,13 +15,11 @@ class ItemsController < ApplicationController
   def create
   make_active_hash
   @item = Item.new(item_params)
-
     if @item.save
       redirect_to root_path(@item)
     else
       render :new
     end
-  
   end
 
   def show
@@ -36,13 +34,20 @@ class ItemsController < ApplicationController
 
   def update
     make_active_hash
-
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end
+  end
 
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
